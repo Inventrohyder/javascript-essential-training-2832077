000398dfb2bdc14b97be313705f4cb8ee046d0e6
Haitham Alhad Hyder <hyder@adobe.com>
2021-06-03 19:48:21 -0400

    ✨ feat: add custom methods

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -16,11 +16,44 @@ const backpack = {
     left: 26,
     right: 26,
   },
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
+  changeName: function (name) {
+    this.name = name;
   },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  adjustVolume: function (volume) {
+    this.volume = volume;
+  },
+  changeColor: function (color) {
+    this.color = color;
+  },
+  adjustPocketNum: function (pocketNum) {
+    this.pocketNum = pocketNum;
+  },
 };
+
+console.log("Object before:", backpack);
+
+console.log("Name before:", backpack.name);
+backpack.changeName("Today's backpack");
+console.log("Name after:", backpack.name);
+
+console.log("Straps before:", backpack.strapLength);
+backpack.newStrapLength(10, 10);
+console.log("Straps after:", backpack.strapLength);
+
+console.log("Volume before:", backpack.volume);
+backpack.adjustVolume(10);
+console.log("Volume after:", backpack.volume);
+
+console.log("Color before:", backpack.color);
+backpack.changeColor("Pink");
+console.log("Color after:", backpack.color);
+
+console.log("PocketNum before:", backpack.pocketNum);
+backpack.adjustPocketNum(4);
+console.log("PocketNum after:", backpack.pocketNum);
+
+console.log("Object after:", backpack);
